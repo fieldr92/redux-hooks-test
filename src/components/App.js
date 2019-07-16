78850ad684c6a718f3021051508fdce8c8e50963
fieldr <fieldr92@gmail.com>
2019-07-16 15:59:12 +0100

    Refactor App

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from 'actions';
 
-const incrementCount = (dispatch, count) => dispatch(increment(count));
-const decrementCount = (dispatch, count) => dispatch(decrement(count));
-
 const App = () => {
   const count = useSelector(state => state.test);
   const dispatch = useDispatch();
   return (
     <>
-      <p>{count}</p>
-      <button onClick={() => incrementCount(dispatch, count)}>+</button>
-      <button onClick={() => decrementCount(dispatch, count)}>-</button>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment(count))}>+</button>
+      <button onClick={() => dispatch(decrement(count))}>-</button>
     </>
   );
 };
