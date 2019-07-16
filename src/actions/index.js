1353c1c4d120d8ca0a8fca169119face645dd068
fieldr <fieldr92@gmail.com>
2019-07-16 15:56:32 +0100

    Increment / decrement actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 import { TEST } from 'actions/types';
 
-export const testFunction = count => ({ type: TEST, payload: count + 1 });
+export const increment = count => ({ type: TEST, payload: count + 1 });
+export const decrement = count => ({ type: TEST, payload: count - 1 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { testFunction } from 'actions';
+import { increment, decrement } from 'actions';
 
-const incrementCount = (dispatch, count) => dispatch(testFunction(count));
+const incrementCount = (dispatch, count) => dispatch(increment(count));
+const decrementCount = (dispatch, count) => dispatch(decrement(count));
 
 const App = () => {
   const count = useSelector(state => state.test);
   const dispatch = useDispatch();
-  return <button onClick={() => incrementCount(dispatch, count)}>{count}</button>;
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => incrementCount(dispatch, count)}>+</button>
+      <button onClick={() => decrementCount(dispatch, count)}>-</button>
+    </>
+  );
 };
 
 export default App;
